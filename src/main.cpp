a3dd5a31bc1cc844e53dc0952cf263474ad21b7c
honey the codewitch <gaztoast@gmail.com>
2022-07-13 06:40:19 -0700

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -30,14 +30,11 @@ constexpr static const int8_t spi_pin_miso = 19;
 using bus_t = tft_spi_ex<spi_host, 
                         lcd_pin_cs, 
                         spi_pin_mosi, 
-                        spi_pin_miso, 
+                        -1, 
                         spi_pin_clk, 
                         SPI_MODE0,
                         false, 
                         320 * 240 * 2 + 8, 2>;
-// can be simplified since the M5 Stack
-// uses the default VSPI pins:
-// using bus_t = tft_spi<spi_host,lcd_pin_cs,SPI_MODE0,320*240*2+8,2>;
 
 using lcd_t = ili9342c<lcd_pin_dc, 
                       lcd_pin_rst, 
@@ -45,8 +42,7 @@ using lcd_t = ili9342c<lcd_pin_dc,
                       bus_t, 
                       1, 
                       true, 
-                      400, 
-                      200>;
+                      400>;
 
 using color_t = color<typename lcd_t::pixel_type>;
 
